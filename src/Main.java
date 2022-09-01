77dc0ef5af9d321eee4130a2fe1f9b1997ba82a4
Nyenskans <katarina.susic049@gmail.com>
2022-09-01 13:58:15 +0200

    planet1 added and is error free

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,11 @@ public class Main {
         omaro.name="ferro";
         omaro.radius=1234455;
         omaro.color="pink";
+
+        //        developing planet1
+        Planet1 superplanet=new Planet1();
+        superplanet.color="magenta";
+        superplanet.name="hash";
+        superplanet.size="545488km";
     }
     }
