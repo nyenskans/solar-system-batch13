813ae2ed8f291c7bfce2bbc691fe6a28ce02290c
Nyenskans <katarina.susic049@gmail.com>
2022-09-01 14:04:12 +0200

    feature for planet2 has been added with 0 errors

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,10 @@ public class Main {
         omaro.name="ferro";
         omaro.radius=1234455;
         omaro.color="pink";
+        //    planet2
+        Planet2 superwomen = new Planet2();
+        superwomen.size="766585857km";
+        superwomen.name="pliea";
+        superwomen.color="green";
     }
     }
