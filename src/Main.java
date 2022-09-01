336b2acaef2013e9b4345ce61a1d4c45066572e9
Nyenskans <katarina.susic049@gmail.com>
2022-09-01 13:02:52 +0200

    Solar System Basics are ready

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        SolarSystem inception = new SolarSystem();
+        inception.Sun = 1;
+        inception.Moon = 4;
+        inception.Planets = 2;
+
+        System.out.println("the basic structure is ready ");
+
+    }
+}
